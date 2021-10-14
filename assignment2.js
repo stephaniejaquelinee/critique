37df0b07a8c242369c0014598e3442a25f6749dc
stephaniejaquelinee <91268437+stephaniejaquelinee@users.noreply.github.com>
2021-10-14 14:01:16 +0700

    Add files via upload

---
+++ assignment2.js
@@ -0,0 +1,28 @@
+var specVis1 = "./econ.vg.json";
+var specVis2= "./rangedDotPlot.vg.json";
+var specVis3= "./hw10.vg.json";
+var specVis4= "./visualization.vg.json"
+var specVis5= "./bar1.vg.json";
+var specVis6= "./bar2.vg.json";
+var specVis7= "./bar3.vg.json"
+vegaEmbed("#vis1", specVis1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed('#vis2', specVis2, {"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
+vegaEmbed('#vis3', specVis3, {"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
+vegaEmbed('#vis4', specVis4, {"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
+vegaEmbed('#vis5', specVis5, {"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
+vegaEmbed('#vis6', specVis6, {"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
+vegaEmbed('#vis7', specVis7, {"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
